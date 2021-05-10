646f23444a8a9d0e949a08b8b625808a2c8b2d72
tif-calin <culitif@tuta.io>
2021-05-10 06:51:23 -0700

    fix userId on get /api/books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,50 +2,116 @@ import app from '../lib/app.js';
 import supertest from 'supertest';
 import client from '../lib/client.js';
 import { execSync } from 'child_process';
+import { describe, expect, test, it, beforeAll } from '@jest/globals';
 
 const request = supertest(app);
 
 describe('API Routes', () => {
 
   const expected = [
-    { 'id':expect.any(Number), 'isbn13':'978-0226001012', 'title':'Chaos of Disciplines', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/51FYOr6-voL._SX323_BO1,204,203,200_.jpg', 'year':2001 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-0226001012', 
+      'title':'Chaos of Disciplines', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/51FYOr6-voL._SX323_BO1,204,203,200_.jpg', 
+      'year':2001,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-0822325932', 'title':'Reyita', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/512SNPDXMHL._SX320_BO1,204,203,200_.jpg', 'year':2000 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-0822325932', 
+      'title':'Reyita', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/512SNPDXMHL._SX320_BO1,204,203,200_.jpg', 
+      'year':2000,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-1479886753', 'title':'Fearing the Black Body', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/51kAu+goXLL._SX331_BO1,204,203,200_.jpg', 'year':2019 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-1479886753', 
+      'title':'Fearing the Black Body', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/51kAu+goXLL._SX331_BO1,204,203,200_.jpg', 
+      'year':2019,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-0684838915', 'title':'The Trouble with Testosterone', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/41tL2psyniL._SX325_BO1,204,203,200_.jpg', 'year':1998 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-0684838915', 
+      'title':'The Trouble with Testosterone', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/41tL2psyniL._SX325_BO1,204,203,200_.jpg', 
+      'year':1998,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-1947534087', 'title':'Dark Emu', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/514mhdNEEEL._SX319_BO1,204,203,200_.jpg', 'year':2018 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-1947534087', 
+      'title':'Dark Emu', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/514mhdNEEEL._SX319_BO1,204,203,200_.jpg', 
+      'year':2018,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-1612191294', 'title':'Debt', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/41Hh11WkivL._SX331_BO1,204,203,200_.jpg', 'year':2011 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-1612191294', 
+      'title':'Debt', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/41Hh11WkivL._SX331_BO1,204,203,200_.jpg', 
+      'year':2011,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-0691178325', 'title':'The Mushroom at the End of the World', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/51H+mQBE3JL._SX329_BO1,204,203,200_.jpg', 'year':2017 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-0691178325', 
+      'title':'The Mushroom at the End of the World', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/51H+mQBE3JL._SX329_BO1,204,203,200_.jpg', 
+      'year':2017,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-0465094271', 'title':'The Master Algorithm', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/51hx36lKe6L._SX331_BO1,204,203,200_.jpg', 'year':2015 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-0465094271', 
+      'title':'The Master Algorithm', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/51hx36lKe6L._SX331_BO1,204,203,200_.jpg', 
+      'year':2015,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-0679776390', 'title':'The Spell of the Sensuous', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/511reY7gVKL._SX322_BO1,204,203,200_.jpg', 'year':1997 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-0679776390', 
+      'title':'The Spell of the Sensuous', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/511reY7gVKL._SX322_BO1,204,203,200_.jpg', 
+      'year':1997,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-1771642484', 'title':'The Hidden Life of Trees', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/41uEMy1V0JL._SX348_BO1,204,203,200_.jpg', 'year':2015 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-1771642484', 
+      'title':'The Hidden Life of Trees', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/41uEMy1V0JL._SX348_BO1,204,203,200_.jpg', 
+      'year':2015,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-0871568779', 'title':'The Unsettling of America', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/51qxoAVZ1xL._SX321_BO1,204,203,200_.jpg', 'year':1977 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-0871568779', 
+      'title':'The Unsettling of America', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/51qxoAVZ1xL._SX321_BO1,204,203,200_.jpg', 
+      'year':1977,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-0963009609', 'title':'PiHKAL', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/41LR44C2IjL._SX331_BO1,204,203,200_.jpg', 'year':1991 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-0963009609', 
+      'title':'PiHKAL', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/41LR44C2IjL._SX331_BO1,204,203,200_.jpg', 
+      'year':1991,
+      userId:expect.any(Number) 
     }, 
-    { 'id':expect.any(Number), 'isbn13':'978-0963009692', 'title':'TiHKAL', 
-      'image':'https://images-na.ssl-images-amazon.com/images/I/41HHS2jXbDL._SX331_BO1,204,203,200_.jpg', 'year':2002 
+    { 
+      'id':expect.any(Number), 
+      'isbn13':'978-0963009692', 
+      'title':'TiHKAL', 
+      'image':'https://images-na.ssl-images-amazon.com/images/I/41HHS2jXbDL._SX331_BO1,204,203,200_.jpg', 
+      'year':2002,
+      userId:expect.any(Number) 
     }
   ];
 
@@ -54,7 +120,8 @@ describe('API Routes', () => {
     isbn13: '978-0745651903',
     title: 'The Tyranny of Science',
     image: 'https://images-na.ssl-images-amazon.com/images/I/41hk8+-IhCL._SX317_BO1,204,203,200_.jpg',
-    year: 2011
+    year: 2011,
+    userId:expect.any(Number) 
   };
 
   const newBook2 = {
@@ -62,19 +129,22 @@ describe('API Routes', () => {
     isbn13: '978-1931498234',
     title: 'Wild Fermentation',
     image: 'https://images-na.ssl-images-amazon.com/images/I/51BbudCTzCL._SX348_BO1,204,203,200_.jpg',
-    year: 2003
+    year: 2003,
+    userId:expect.any(Number) 
   };
 
   describe('/api/books', () => {
+
+    let user;
 
     afterAll(async () => {
       return client.end();
     });
 
     beforeAll(async () => {
-      execSync('npm run setup-db');
+      execSync('npm run recreate-tables');
   
-      /*const response = await request
+      const response = await request
         .post('/api/auth/signup')
         .send({
           name: 'Me the User',
@@ -82,11 +152,14 @@ describe('API Routes', () => {
           password: 'password'
         });
   
-      expect(response.status).toBe(200); */
+      expect(response.status).toBe(200); 
+
+      user = response.body;
     });
 
     // get list of books
     it('GET /api/books', async () => {
+      execSync('npm run setup-db');
       const response = await request.get('/api/books');
 
       expect(response.status).toBe(200);
@@ -95,7 +168,7 @@ describe('API Routes', () => {
     });
 
     // get a specific book by id
-    it.skip('GET /api/books/:id', async () => {
+    it('GET /api/books/:id', async () => {
       const response = await request.get('/api/books/2');
 
       expect(response.status).toBe(200);
