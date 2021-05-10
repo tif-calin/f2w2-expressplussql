3b9669afd27d95db37f02f297b82db5c3f3884c1
tif-calin <culitif@tuta.io>
2021-05-09 21:17:43 -0700

    modify drop tables for users table

--- data/drop-tables.js
+++ data/drop-tables.js
@@ -9,6 +9,7 @@ async function run() {
     
     await client.query(`
       DROP TABLE IF EXISTS books;
+      DROP TABLE IF EXISTS users CASCADE;
     `);
 
     console.log('drop tables complete');
