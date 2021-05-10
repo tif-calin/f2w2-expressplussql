a6fb45d2b5ea1c068c211dc4ad9cca30572637d0
tif-calin <culitif@tuta.io>
2021-05-10 05:59:44 -0700

    add users data for seed data

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -2,6 +2,7 @@
 import client from '../lib/client.js';
 // import our seed data:
 import books from './books.js';
+import users from './users.js';
 
 run();
 
@@ -9,13 +10,23 @@ async function run() {
 
   try {
 
+    const data = await Promise.all(users.map(user => {
+      return client.query(`
+        INSERT INTO users (name, email, password)
+        VALUES ($1, $2, $3)
+        RETURNING *;
+      `, [user.name, user.email, user.password]);
+    }));
+
+    const user = data[0].rows[0];
+
     await Promise.all(
       books.map(book => {
         return client.query(`
           INSERT INTO books (isbn13, title, image, year, user_id)
           VALUES ($1, $2, $3, $4, $5);
         `,
-        [book.isbn13, book.title, book.image, book.year, 0]);
+        [book.isbn13, book.title, book.image, book.year, user.id]);
       })
     );
 
---
+++ data/users.js
@@ -0,0 +1,9 @@
+const users = [
+  {
+    name: 'seed',
+    email: 'seed@seed.org',
+    password: 'password'
+  }
+];
+
+export default users;
